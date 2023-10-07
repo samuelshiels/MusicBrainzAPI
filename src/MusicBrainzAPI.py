326be1cb4c95ddea2e1dc108db5ba7cb89983cea
Samuel <samuel.shiels@googlemail.com>
2023-10-07 09:54:04 +0100

    2023-10-07 09:54:03

--- src/MusicBrainzAPI.py
+++ src/MusicBrainzAPI.py
@@ -1,17 +1,18 @@
+import logging
+import os
 import getRestData
 import json
 from RESTObject import RESTObject as ro
 from pathlib import Path
 
 appName = 'MusicBrainzAPI'
-#sleep time, in ms, between any api calls, MB allow up to 50/s but we only need 10 for now. 2023 - Re-read rules and its 1/s
+# sleep time, in ms, between any api calls, MB allow up to 50/s but we only need 10 for now. 2023 - Re-read rules and its 1/s
 sleep = 1100
-#cache time, in mins, for api calls to refresh cached data, we keep anything for 1 week since I have some obscure bands that may suddenly get their catalog updated
+# cache time, in mins, for api calls to refresh cached data, we keep anything for 1 week since I have some obscure bands that may suddenly get their catalog updated
 time = 10800
-#root cache directory
+# root cache directory
 cache = f'{Path.home}/.cache/{appName}/'
 
-import os
 ae = {}
 ae['app_name'] = appName
 ae['version'] = '0.1.0'
@@ -21,53 +22,59 @@ ae['sleep'] = sleep
 ae['time'] = time
 
 rootURL = 'https://musicbrainz.org/ws/2/'
-#Provide a User-Agent so they can contact us if something goes wrong
+# Provide a User-Agent so they can contact us if something goes wrong
 userAgent = 'MusicBrainzAPI/0.1 (https://github.com/samuelshiels/MusicBrainzAPI)'
 
-import logging
 logging.basicConfig(
-format='%(asctime)s | %(levelname)s | %(message)s', level=logging.DEBUG)
-debug = True
+    format='%(asctime)s | %(levelname)s | %(message)s', level=logging.DEBUG)
+debug = False
+
+
 def __debugMessage(message):
-	global debug
-	if debug:
-		logging.debug(str(message))
+    if debug:
+        logging.debug(str(message))
 
 
 def __buildHeaderObj():
-	global userAgent
-	headers = {}
-	headers['User-Agent'] = userAgent
-	#we want responses in json, because fuck xml
-	headers['Accept'] = 'application/json'
-	return headers
+    global userAgent
+    headers = {}
+    headers['User-Agent'] = userAgent
+    # we want responses in json, because fuck xml
+    headers['Accept'] = 'application/json'
+    return headers
 
-def __runRest(e, p, o, c, log=False):
-	global rootURL, cache, time, sleep
-	restObj = ro(operation='get', endpoint=f'{rootURL}{e}',params=p,headers=__buildHeaderObj(),payload={})
-	config = {}
 
-	config['output'] = o + '.json'
-	config['cache'] = cache + c
-	config['time'] = time
-	config['sleep'] = sleep
-	config['rest'] = restObj
-	return getRestData.execute(config, log)
+def __runRest(e, p, o, c):
+    __debugMessage(f"__runRest")
+    global rootURL, cache, time, sleep
+    restObj = ro(operation='get', endpoint=f'{rootURL}{e}',
+                 params=p, headers=__buildHeaderObj(), payload={})
+    config = {}
+
+    config['output'] = o + '.json'
+    config['cache'] = ae['cache_dir'] + c
+    config['time'] = time
+    config['sleep'] = sleep
+    config['rest'] = restObj
+    __debugMessage(f"{config}")
+    return getRestData.execute(config)
+
 
 def getArtistByMBID(mbid, log=False):
-	return __runRest(f'artist/{mbid}', {'inc':'aliases'}, mbid, 'artist', log)
+    __debugMessage(f"getArtistByMBID")
+    return __runRest(f'artist/{mbid}', {'inc': 'aliases'}, mbid, 'artist')
 
 
+def getReleasesByArtist(mbid):
+    """contains property 'release-groups'"""
+    return __runRest(f'artist/{mbid}', {'inc': 'aliases+release-groups'}, mbid, 'releases')
 
-def getReleasesByArtist(mbid, log=False):
-	"""contains property 'release-groups'"""
-	return __runRest(f'artist/{mbid}', {'inc':'aliases+release-groups'}, mbid, 'releases', log)
 
-def getReleaseTitlesByArtist(mbid, log=False):
-	"""runs a getReleasesByArtist but returns the release titles of the response"""
-	retVal = []
-	artistReleases = getReleasesByArtist(mbid)
-	response = json.loads(artistReleases)
-	for release in response['release-groups']:
-		retVal.append(release['title'])
-	return retVal
+def getReleaseTitlesByArtist(mbid):
+    """runs a getReleasesByArtist but returns the release titles of the response"""
+    retVal = []
+    artistReleases = getReleasesByArtist(mbid)
+    response = json.loads(artistReleases)
+    for release in response['release-groups']:
+        retVal.append(release['title'])
+    return retVal
--- tests/test_musicbrainzapi.py
+++ tests/test_musicbrainzapi.py
@@ -3,3 +3,10 @@ from src import MusicBrainzAPI as mb
 def test_active():
 	assert(1 == 1)
 	assert(True is not False)
+
+def test_getArtistByMBID():
+	artistMBIDList = ['35f92c4a-69d0-4ed1-ab9e-05259db89d14']
+	artist = mb.getArtistByMBID(artistMBIDList[0])
+	assert(artist["type"] == "Group")
+	assert(artist['sort-name'] == "At the Gates")
+	releases = mb.getReleaseTitlesByArtist(artistMBIDList[0])
